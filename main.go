2bd5a1ea53080ffc7894b53ff3d2478fcd563a38
yufernando <yufernando@gmail.com>
2020-04-09 03:18:40 -0400

    Adds container hostname in script

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 	"syscall"
 )
 
-// docker run <container> cmd args
-// go     run main.go run cmd args
+// docker		  run <image> cmd args
+// go run main.go run         cmd args
 
 func main() {
 	switch os.Args[1] {
@@ -22,20 +22,29 @@ func main() {
 }
 
 func run() {
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+		Cloneflags:   syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
 }
 
 func child() {
-	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
+
+	// Set the hostname in the container
+	must(syscall.Sethostname([]byte("container")))
+	must(syscall.Chroot("/home/fer/DataScience/rootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -46,11 +55,9 @@ func child() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
-	must(syscall.Chroot("/home/fer/DataScience/rootfs"))
-	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-
 	must(cmd.Run())
+
+	syscall.Unmount("/proc", 0)
 }
 
 func must(err error) {
