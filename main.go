d3ab672979e4497259d9a3066ee75a3784865aa3
yufernando <yufernando@gmail.com>
2020-04-09 01:30:21 -0400

    First version of container

--- main.go
+++ main.go
@@ -23,13 +23,13 @@ func run() {
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
-	cmd.Stout = os.Stdout
-	cmd.Sterr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
 	must(cmd.Run())
 }
 
-func mist(err error) {
+func must(err error) {
 	if err != nil {
 		panic(err)
 	}
